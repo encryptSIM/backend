239b9d0d74760a217332713c1792a84b070be79e
Jack Kitto <jack.kitto@gmail.com>
2025-08-15 17:24:02 +0900

    feat: add Coverage type to represent operator coverages

--- src/airalo-api/types.ts
+++ src/airalo-api/types.ts
@@ -3,3 +3,4 @@ import { paths } from "./schema";
 export type Usage = NonNullable<NonNullable<paths['/v2/sims/{sim_iccid}/usage']['get']['responses']['200']['content']['application/json']>['data']>
 export type GetPackagesResponse = paths["/v2/packages"]["get"]["responses"]["200"]["content"]["application/json"];
 export type Operator = NonNullable<NonNullable<GetPackagesResponse["data"]>[number]["operators"]>[number];
+export type Coverage = NonNullable<Operator["coverages"]>[number];
