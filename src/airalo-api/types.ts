4b9c21da92d3f0934bef72992e1c7692a9bc1a7b
Jack Kitto <jack.kitto@gmail.com>
2025-08-15 17:23:40 +0900

    feat: add type definition for GetPackagesResponse

--- src/airalo-api/types.ts
+++ src/airalo-api/types.ts
@@ -1,3 +1,4 @@
 import { paths } from "./schema";
 
 export type Usage = NonNullable<NonNullable<paths['/v2/sims/{sim_iccid}/usage']['get']['responses']['200']['content']['application/json']>['data']>
+export type GetPackagesResponse = paths["/v2/packages"]["get"]["responses"]["200"]["content"]["application/json"];
